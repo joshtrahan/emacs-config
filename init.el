19727fd8abff3eacc4f50c1336a8e01e3ace02b4
Josh Trahan <josht0288@gmail.com>
2017-06-22 16:48:48 -0500

    change theme

--- init.el
+++ init.el
@@ -28,7 +28,9 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (misterioso)))
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
  '(package-selected-packages
    (quote
     (company-math helm-projectile org-projectile projectile company-emacs-eclim company-shell company helm-pydoc magit helm htmlize monokai-theme lua-mode multiple-cursors)))
